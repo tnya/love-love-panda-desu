4199902c8d35a2502a16c39f75bda3222451c56b
Tanya Ramirez <tcramire@gmail.com>
2017-05-19 00:46:36 -0400

    falta pulir detalles minimos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+
+/* HACER forEach y crear un div para almacenar nuevas fotos */
+var photos = document.getElementsByClassName('pandas')[0];
+
+pictures.forEach(function(elements){
+	var img = document.createElement('img') // <img/>		
+	img.setAttribute("src",elements);
+	img.setAttribute("class","cuatropandas");
+	photos.appendChild(img);	
+	})
+
+
